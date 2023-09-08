4830e1ec1894a812a7778c71121acdef22d559f3
Siraphob1 <Sirapop_t@hotmail.com>
2023-09-08 17:44:11 +0700

    react8 excercise3

--- react-8/exercise-3/src/App.jsx
+++ react-8/exercise-3/src/App.jsx
@@ -1,5 +1,49 @@
 import React from "react";
 
 // class App here
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {color:''};
+    }
 
+    setColor(newcolor){
+        this.setState({color:newcolor})
+    }
+
+    render(){
+        return (
+            <>
+              {this.state.color === "yellow" && (
+                <div
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    margin: "12px",
+                    background: "yellow",
+                  }}
+                >
+                  YELLOW
+                </div>
+              )}
+        
+              {this.state.color === "blue" && (
+                <div
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    margin: "12px",
+                    background: "blue",
+                  }}
+                >
+                  BLUE
+                </div>
+              )}
+        
+              <button onClick={() => this.setColor("yellow")}>Show Yellow</button>
+              <button onClick={() => this.setColor("blue")}>Show Blue</button>
+            </>
+          );
+    }
+}
 export default App;
